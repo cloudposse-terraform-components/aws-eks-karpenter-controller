3d3642271d2a00d0b001b2ba4c75c7e025c61669
Ben <ben.smith.developer@gmail.com>
2026-03-12 14:40:42 -0700

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/remote-state.tf
+++ src/remote-state.tf
@@ -1,11 +1,11 @@
 variable "eks" {
   type = object({
-    eks_cluster_id                   = optional(string, null)
-    eks_cluster_arn                  = optional(string, null)
-    eks_cluster_endpoint             = optional(string, null)
+    eks_cluster_id                         = optional(string, null)
+    eks_cluster_arn                        = optional(string, null)
+    eks_cluster_endpoint                   = optional(string, null)
     eks_cluster_certificate_authority_data = optional(string, null)
-    eks_cluster_identity_oidc_issuer = optional(string, null)
-    karpenter_iam_role_arn           = optional(string, null)
+    eks_cluster_identity_oidc_issuer       = optional(string, null)
+    karpenter_iam_role_arn                 = optional(string, null)
   })
   description = "EKS cluster outputs. When set, bypasses remote-state lookup of eks/cluster."
   default     = null
@@ -22,11 +22,11 @@ module "eks" {
 
   # Attempt to allow this component to be deleted from Terraform state even after the EKS cluster has been deleted
   defaults = {
-    eks_cluster_id                   = try(var.eks.eks_cluster_id, "deleted")
-    eks_cluster_arn                  = try(var.eks.eks_cluster_arn, "deleted")
-    eks_cluster_identity_oidc_issuer = try(var.eks.eks_cluster_identity_oidc_issuer, "deleted")
-    karpenter_iam_role_arn           = try(var.eks.karpenter_iam_role_arn, "deleted")
-    eks_cluster_endpoint             = try(var.eks.eks_cluster_endpoint, null)
+    eks_cluster_id                         = try(var.eks.eks_cluster_id, "deleted")
+    eks_cluster_arn                        = try(var.eks.eks_cluster_arn, "deleted")
+    eks_cluster_identity_oidc_issuer       = try(var.eks.eks_cluster_identity_oidc_issuer, "deleted")
+    karpenter_iam_role_arn                 = try(var.eks.karpenter_iam_role_arn, "deleted")
+    eks_cluster_endpoint                   = try(var.eks.eks_cluster_endpoint, null)
     eks_cluster_certificate_authority_data = try(var.eks.eks_cluster_certificate_authority_data, null)
   }
 
